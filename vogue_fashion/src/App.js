25d8be510dde67d521efd2e194a59d7dc8a474d7
Neerav-Khatri <khaneerav6337@gmail.com>
2023-01-21 04:24:36 +0530

    Day_4

--- vogue_fashion/src/App.js
+++ vogue_fashion/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import AllRoutes from "./Routes/AllRoutes";
+import ProductsPage from './Routes/ProductsPage';
 
 function App() {
+  document.title="Vogue Fashion";
+
   return (
     <div className="App">
-        <AllRoutes />
+        {/* <AllRoutes /> */}
+        <ProductsPage />
     </div>
   );
 }
